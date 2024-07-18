8c4d5fdc946b0dece4eee2e400db2629b814678a
xriera-c <149150079+xrierac@users.noreply.github.com>
2024-07-18 16:16:25 +0300

    Add intersection functions

--- Makefile
+++ Makefile
@@ -18,7 +18,9 @@ LIBS	:= $(LIBMLX)/build/libmlx42.a -Iinclude -ldl -lglfw -pthread -lm
 			
 SRCS	:= $(SRCDIR)/main.c $(SRCDIR)/definitions.c \
 			$(SRCDIR)/tuple_operations_one.c \
-			$(SRCDIR)/tuple_operations_two.c
+			$(SRCDIR)/tuple_operations_two.c \
+			$(SRCDIR)/rays.c $(SRCDIR)/sphere.c \
+			$(SRCDIR)/intersections.c
 OBJS	:= ${SRCS:.c=.o}
 
 #all: libmlx $(LIBFT) $(NAME)
---
+++ src/sphere.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sphere.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/18 10:50:17 by xriera-c          #+#    #+#             */
+/*   Updated: 2024/07/18 15:22:18 by xriera-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/minirt.h"
+
+t_intersections	sp_cross(t_sphere sp, t_ray	r)
+{
+	t_intersections	cross;
+	t_tuple			sp_to_ray;
+	t_tuple			values;
+	float			discriminant;
+
+	cross.count = 0;
+	sp_to_ray = tuple_subs(r.origin, sp.center);
+	values.x = dot_product(r.direction, r.direction);
+	values.y = 2 * dot_product(r.direction, sp_to_ray);
+	values.z = dot_product(sp_to_ray, sp_to_ray) - 1;
+	discriminant = values.y * values.y - (4 * values.x * values.z);
+	if (discriminant >= 0)
+	{
+		cross.t[0] = (-values.y - sqrt(discriminant)) / (2 * values.x);
+		cross.t[1] = (-values.y + sqrt(discriminant)) / (2 * values.x);
+		cross.object[0] = 's';
+		cross.object[1] = 's';
+		cross.count = cross.count + 2;
+	}
+	return (cross);
+}
