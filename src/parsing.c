b7b3994323a50df2e9aca505f9653df2bf7bd25e
eleekku <144835604+eleekku@users.noreply.github.com>
2024-07-25 19:02:51 +0300

    working on validate camera

--- src/parsing.c
+++ src/parsing.c
@@ -1,67 +1,112 @@
 #include "../inc/minirt.h"
 
+void    free_array(char **args)
+{
+    int i;
+
+    i = -1;
+    while(args[++i])
+        free(args[i]);
+    free(args);
+}
+
 void    exit_error(char *msg)
 {
     ft_printf(2, "Error\n%s\n", msg);
     exit(1);
 }
 
+t_bool    validate_values(char *arg)
+{
+    char    **rgb;
+    int     i;
+    int     coma;
+
+    i = 0;
+    coma = 0;
+    while (arg[i])
+    {
+        if (arg[i] == ',')
+            {
+                coma++;
+                i++; 
+            }
+        if ((arg[i] != '.' && arg[i] != '-') && (arg[i] < '0' || arg[i] > '9'))
+            exit_error("Invalid value format\n");
+        i++;
+    }
+    if (coma > 2)
+        exit_error("Invalid egb format");
+    return (TRUE);
+}
+
+char    **safe_split(char string, char separator)
+{
+    char **arr;
+
+    arr = ft_split(string,  separator);
+    if (!arr)
+    {
+        exit(1);
+    }
+}
+
 void    validate_ambient(char **args, t_scene *scene)
 {
         float   lightratio;
-        int     rgb;
+        char    **rgb;
         int     i;
+        float   value;
 
-        i = 2;
-        if (ft_strncmp(args[0], "A", ft_strlen(args[0]) != 0))
-            exit_error("invalid format");
+        i = -1;
         if (!ft_isdigit(args[1][0]))
             exit_error("invalid format");
         lightratio = ft_atof(args[1]);
         if (lightratio < 0.0 || lightratio > 1.0)
             exit_error("invalid ambient light ratio");
-        scene->alightr = ft_atof(args[1]);
-        while (i < 5)
+        scene->alightr = lightratio;
+        validate_values(args[1]);
+        rgb = safe_split(args[1], ",");
+        while (++i < 2)
         {
-            if (!ft_isdigit(args[i][0]))
-                exit_error("invalid ambient rgb value"); 
-            rgb = ft_atoi(args[i]);
-            if (rgb == 0 && args[i][0] != '0' || (rgb > 255 || rgb < 0))
+            value = ft_atoi(rgb[i]);
+            if (!(value >= 0 && value <= 255))
                 exit_error("invalid ambient rgb value");
-            else
-                scene->amcolor[i - 2] = rgb;
-            i++;
-        }       
+            scene->amcolor[i] = value;
+        }
+        if (args[i])
+            exit_error("too many values in ambien rgb");
+        free_array(rgb);      
 }
 
 void    validate_camera(char **args, t_scene *scene)
 {
         int     i;
-        int     coordinate;
+        char    **coordinates;
+        float   value;
 
-        i = 1;
-        if (ft_strncmp(args[0], "C", ft_strlen(args[0]) != 0))
-            exit_error("invalid format");
-        if (!ft_isdigit(args[1][0]))
-            exit_error("invalid format");
-        coordinate = ft_atof(args[1]);
-        while(++i < 4)
-        { 
-            coordinate = ft_atoi(args[i]);
-            if (coordinate == 0 && args[i][0] != '0')
-                exit_error("invalid camera coordinates");               
-            scene->camc[i - 1] = coordinate;
+        i = -1;
+        validate_values(args[1]);
+        coordinates = safe_split(args[1], ',');
+        while (++i < 2)
+        {
+            if (ft_strchr(args[i], '.'))
+                value = ft_atof(args[i]);
+            else
+                value = ft_atoi(args[i]);
+            scene->camc[i] = value;
         }
-            
+        if (args[i])
+            exit_error("too  many values in camera coordinates");
+        free_array(coordinates);    
 }
 
 t_bool  validate_line(char *arg, char **args, t_scene *scene)
 {
-    if (ft_strncmp(arg, "A", 1) == 0)
-    {
+    if (ft_strncmp(arg, "A", ft_strlen(arg)) == 0)
         validate_ambient(args, scene);
-        return(TRUE);
-    if else (ft_strncmp(arg, "C", 1) == 0)
+    else if (ft_strncmp(arg, "C", ft_strlen(arg)) == 0)
+        validate_camera(args, scene);
 
 //    if else ft_strncmp(arg, "L", 1) == 0)
 
